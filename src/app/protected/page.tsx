c245bcb2f9fe7f28c39af043d8de8e7aaaced413
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-03 23:16:11 -0400

    add protected route

---
+++ src/app/protected/page.tsx
@@ -0,0 +1,17 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function ProtectedRoute() {
+  const session = await getServerSession();
+  if (!session || !session.user) {
+    redirect("/api/auth/signin");
+  }
+
+  return (
+    <div>
+      This is a protected route.
+      <br />
+      You will only see this if you are authenticated.
+    </div>
+  );
+}
