e31b4667178a50a2f2a7d4b33d3f8e59b3f39008
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-03 23:06:04 -0400

    create signin signout navbar

---
+++ src/app/components/NavMenu.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const AuthButton = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        {session?.user?.name} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+};
+
+const NavMenu = () => {
+  return (
+    <div>
+      <AuthButton />
+    </div>
+  );
+};
+
+export default NavMenu;
