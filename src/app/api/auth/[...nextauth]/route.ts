b2e10e7eb658f3adb411f7f03111aac51a682d1b
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-06 09:36:04 -0400

    change to google provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,15 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
-export const authoptions = {
+export const authOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
 };
 
-export const handler = NextAuth(authoptions);
+export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- src/app/api/whoAmI/route.tsx
+++ src/app/api/whoAmI/route.tsx
@@ -1,10 +1,10 @@
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-import { authoptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET() {
-  const session = await getServerSession(authoptions);
+  const session = await getServerSession(authOptions);
 
   return NextResponse.json({ name: session?.user?.name ?? "Not Logged In" });
 }
