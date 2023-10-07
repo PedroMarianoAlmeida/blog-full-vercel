dfc0444312233a239c1786f79df3ea17bd347f2c
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-10-06 20:38:04 -0400

    add create post validation

--- src/app/post/new/page.tsx
+++ src/app/post/new/page.tsx
@@ -1,17 +1,28 @@
 import CreatePostForm from "@/components/CreatePostForm";
 import { canUserCreatePostChecker } from "@/services/authServices";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedRoute() {
   const canUserCreatePost = await canUserCreatePostChecker();
 
   if (!canUserCreatePost) {
-    redirect("/");
+    return (
+      <div>
+        <h2>You cannot create a post</h2>
+        <p>
+          Ask to the administration on{" "}
+          <Link href="https://www.linkedin.com/in/pedroprogrammer/">
+            <span className="text-orange-500">Linkedin</span>
+          </Link>
+        </p>
+      </div>
+    );
   }
 
   return (
     <div>
-     <CreatePostForm />
+      <CreatePostForm />
     </div>
   );
 }
